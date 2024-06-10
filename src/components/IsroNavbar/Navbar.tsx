c641230bd8b311a16cd77fa232de6ab3ad8ea28b
TheBug <atharvsathe28704@gmail.com>
2024-06-10 13:04:10 +0530

    Changed Navbar's Z-Index to 10

--- src/components/IsroNavbar/Navbar.tsx
+++ src/components/IsroNavbar/Navbar.tsx
@@ -5,7 +5,7 @@ import NavItems from './NavItems';
 
 function Navbar() {
     return (
-        <nav className=' m-auto w-fit gap-16  flex  items-center'>
+        <nav className=' z-10  m-auto w-fit gap-16  flex  items-center'>
             <Logo />
             <NavItems />
         </nav>
