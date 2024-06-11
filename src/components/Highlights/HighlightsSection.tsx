44015e445d0ddd8db91042d6abbdb6b735bcf6ce
TheBug <atharvsathe28704@gmail.com>
2024-06-11 23:30:19 +0530

    Added font for content, removed google fonts links.

--- src/components/Highlights/HighlightsSection.tsx
+++ src/components/Highlights/HighlightsSection.tsx
@@ -2,7 +2,7 @@ function HighlightsSection() {
     return (
         <section id="highlights-section">
             <div id="carousel-heading">
-                <h1 className=" font-ubuntu">Highlights</h1>
+                <h1 className=" font-space text-4xl ml-4">Highlights</h1>
             </div>
             <div id="carousel">
 
