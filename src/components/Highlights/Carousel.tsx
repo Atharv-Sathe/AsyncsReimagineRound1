a539bd56af4b44582aeffae794d82968c29b495e
TheBug <atharvsathe28704@gmail.com>
2024-06-13 20:59:41 +0530

    Added automatic carousel with user interaction.

---
+++ src/components/Highlights/Carousel.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import "./Carousel.css";
+
+type CarouselProps = {
+  images: {
+    id: number;
+    url: string;
+    title: string;
+    description: string;
+  }[];
+};
+
+const Carousel: React.FC<CarouselProps> = ({ images }) => {
+
+  const [current, setCurrent] = useState(0);
+  const [timeOut, setTimeOut] = useState<number>(0);
+
+  const slideLeft = () => {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  }
+
+  const slideRight = () => {
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  }
+
+  const [autoPlay, setAutoPlay] = useState(true);
+
+  /*
+  Importance of useEffect Cleanup Function in Carousel Component:
+  Here’s what’s happening =>
+  1. Pagination Dots: When a pagination dot is clicked, it sets the current state to the index of the clicked dot. This change in 
+  state triggers a re-render of the carousel, displaying the corresponding image.
+  2. Cleanup Function: The cleanup function provided in the useEffect hook is called every time before the effect runs again, which 
+  includes when the current state changes. This ensures that any ongoing timeout (set for auto-playing the carousel) is cleared, 
+  preventing it from interfering with the manual change caused by clicking a pagination dot.
+  3. Consistent Behavior: By clearing the timeout whenever current changes, you avoid issues like the carousel trying to auto-play to the next image while the user is manually navigating through the images. This makes the behavior of the carousel consistent and predictable.
+  */
+
+  useEffect(() => {
+    if (autoPlay) {
+      const id = setTimeout(() => slideRight(), 2500);
+      setTimeOut(id); // This is a setter function for the state variable timeOut, not to be confused with the setTimeout function.
+    }
+
+    return () => clearTimeout(timeOut);
+  }, [current, autoPlay]);
+
+  const handleMouseEnter = () => {
+    setAutoPlay(false);
+    clearTimeout(timeOut);
+  };
+
+  const hadleMouseLeave = () => setAutoPlay(true);
+
+  return (
+    <div className="carousel flex h-full w-full"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={hadleMouseLeave}
+    >
+      <div className="carousel-wrapper relative w-full h-full">
+        {images.map((img, ind) => (
+          <div key={ind} className={`${ind === current ? "carousel-card carousel-card-active" : "carousel-card"}  `}>
+            <img className="card-image w-full h-full  filter-none object-fill"  src={img.url} alt="carousel" />
+            <div className="card-overlay h-full w-full absolute bg-black bg-opacity-50 flex flex-col justify-center items-center"> 
+              <h2>{img.title}</h2>
+              <p>{img.description}</p>
+            </div>
+          </div>
+        ))}
+        <div className="carousel-arrow-left" onClick={slideLeft}>&lsaquo;</div>
+        <div className="carousel-arrow-right" onClick={slideRight}>&rsaquo;</div>
+        <div className="carousel-pagination absolute left-1/2  flex gap-2">
+          {images.map(( _ , ind) => (
+            <div key={ind} className={`${ind === current ? "pagination-dot pagination-dot-active" : "pagination-dot"}`}
+            onClick={() => setCurrent(ind)}
+            ></div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Carousel;
--- src/components/Highlights/HighlightsSection.tsx
+++ src/components/Highlights/HighlightsSection.tsx
@@ -1,11 +1,14 @@
 "use client";
 // import React from "react";
 import { SparklesCore } from "../Effects/Sparkles";
+import Carousel from "./Carousel";
+import {ImgData} from "./ImgData";
+
 
 function HighlightsSection() {
     return (
-        <section id="highlights-section relative">
-            <div className="w-full h-full absolute">
+        <section id="highlights-section" className="relative w-full h-[100vh]">
+            <div className="sparkles w-full h-full absolute z-0">
                 <SparklesCore
                 id="tsparticlesfullpage"
                 background="transparent"
@@ -16,12 +19,13 @@ function HighlightsSection() {
                 particleColor="#FFFFFF"
                 />
             </div>
-            <div id="carousel-heading" className=" relative left-20">
-                <h1 className="font-space text-5xl relative top-20">Highlights</h1>
+            <div id="carousel-heading" className=" relative z-10  left-20 top-20">
+                <h1 className="font-space text-5xl text-white">Highlights</h1>
             </div>
-            <div id="carousel">
-                
+            <div id="carousel-content" className="relative z-10 left-1/4 top-32 h-[65%] w-[50%] max-h-96 text-white">
+                <Carousel images={ImgData}/>
             </div>
+
         </section>
     );
 }
