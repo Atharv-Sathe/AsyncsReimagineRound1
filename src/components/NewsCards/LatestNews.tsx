c603701598f8e00759018377c0a15396fdaa5210
Sujoy De <sujoydeofficial@gmail.com>
2024-06-13 22:20:46 +0530

    re-add: latest news

--- src/components/NewsCards/LatestNews.tsx
+++ src/components/NewsCards/LatestNews.tsx
@@ -4,7 +4,7 @@ import CardItems from "./CardItems";
 export default function LatestNews() {
   return (
     <div className="flex flex-col">
-      <h2 className="text-4xl p-4 font-space">Latest News</h2>
+      <h2 className="text-5xl text-white pl-20 pt-20 pb-10 font-space">Latest News</h2>
       <CardItems items={NewsItems} />
     </div>
   );
