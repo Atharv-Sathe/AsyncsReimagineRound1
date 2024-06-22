c72e222406166e935d688601066af7e1e08bb3f1
TheBug <atharvsathe28704@gmail.com>
2024-06-22 18:29:43 +0530

    Added Scroll to Start arrow!

--- src/components/NewsCards/LatestNews.tsx
+++ src/components/NewsCards/LatestNews.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NewsItems } from "../../utils/data";
 import CardItem from "./CardItem";
 import { cn } from "../../utils/cn";
+import './LatestNews.css';
 
 export default function LatestNews() {
   const containerRef = React.useRef<HTMLDivElement>(null);
@@ -32,6 +33,36 @@ export default function LatestNews() {
     }
   }
 
+  const scrollToStart = () => {
+    if (scrollerRef.current) {
+      // Stop the animation
+      scrollerRef.current.style.animationPlayState = 'paused';
+
+      // Reset scroll position
+      scrollerRef.current.scrollLeft = 0;
+
+      // Reset the animation by removing it and then re-adding it
+      const originalAnimation = scrollerRef.current.style.animation;
+      scrollerRef.current.style.animation = 'none';
+
+      // Force reflow to ensure the animation reset is applied
+      // This is a trick to force the browser to recognize the change and apply it immediately
+      void scrollerRef.current.offsetWidth;
+
+      // Delay before resuming the animation
+      setTimeout(() => {
+        if (scrollerRef.current) {
+          // Re-apply the original animation to effectively restart it
+          scrollerRef.current.style.animation = originalAnimation;
+
+          // Resume the animation
+          scrollerRef.current.style.animationPlayState = 'running';
+        }
+      }, 100); // A delay of 100 milliseconds for visual effect
+    }
+  };
+
+
   return (
     <div
       className="flex flex-col scroller relative z-20 w-full overflow-hidden"
@@ -40,9 +71,22 @@ export default function LatestNews() {
       <h2 className="text-5xl text-white pl-20 pt-20 pb-10 font-space">
         Latest News
       </h2>
+      <button className="scroll-to-start-btn arrowMove absolute top-28 right-10 h-9 w-9 text-white"
+      onClick={scrollToStart}
+      >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" style={{ width: '100%', height: '100%' }}>
+        <title>4-Arrow Left</title>
+        <g id="_4-Arrow_Left" data-name="4-Arrow Left">
+          <path
+            fill="white"
+            d="M32,15H3.41l8.29-8.29L10.29,5.29l-10,10a1,1,0,0,0,0,1.41l10,10,1.41-1.41L3.41,17H32Z"
+          />
+        </g>
+      </svg> 
+      </button>
       <ul
         className={cn(
-          "flex flex-row min-w-full w-max hover:overflow-x-auto no-scrollbar",
+          "flex flex-row min-w-full w-max overflow-x-auto no-scrollbar",
           start && "animate-scroll ",
           "hover:[animation-play-state:paused]",
           "scroll-smooth"
