7648a1b00f956a746bc4981e013022e4de4566ed
Ayush Pandey <ayush12122003@gmail.com>
2024-06-23 13:09:37 +0530

    loadingModel

--- src/components/modelsSection/model.tsx
+++ src/components/modelsSection/model.tsx
@@ -2,29 +2,38 @@ import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Satellite from "../../components/modelsSection/Sattelite";
+import { HeroScrollDemo } from "../missions/pageMain";
 
 export default function Model() {
   return (
-    <div className="flex flex-col w-[50%] h-[100vh]">
-      <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-        <Canvas className="h-full w-full p-2">
-          <ambientLight intensity={1.5} />
-          <PerspectiveCamera 
-            makeDefault 
-            position={[10, 0, 0]} 
-            fov={50} 
-            near={0.2} 
-            far={1000} 
-          />
-          <OrbitControls />
-          <Suspense fallback={null}>
-            <group position={[0, 0, 0]} scale={[0.7,0.7,0.7]}>
-              <Satellite />
-            </group>
-          </Suspense>
-          <Environment preset="sunset" />
-        </Canvas>
-      </div>
+    <>
+    <div className="flex flex-col">
+      <h2 className="text-5xl text-white pl-20 pt-20 pb-10 font-space">Missions</h2>
     </div>
+    <div className="flex h-screen mb-[2rem]">
+    <div className="w-1/3 h-full">
+      <Canvas className="h-full w-full p-2 overflow-hidden relative">
+        <ambientLight intensity={1.5} />
+        <PerspectiveCamera 
+          makeDefault 
+          position={[10, 0, 0]} 
+          fov={50} 
+          near={0.2} 
+          far={1000} 
+        />
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <group position={[0, 0, 0]} scale={[0.7, 0.7, 0.7]}>
+            <Satellite />
+          </group>
+        </Suspense>
+        <Environment preset="sunset" />
+      </Canvas>
+    </div>
+    <div className="w-2/3 h-full overflow-hidden">
+      <HeroScrollDemo />
+    </div>
+  </div>
+  </>
   );
 }
