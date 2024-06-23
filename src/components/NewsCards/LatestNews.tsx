87f21bc888b45afea056d92ca8712ba65cf6493c
Sujoy De <sujoydeofficial@gmail.com>
2024-06-23 12:11:22 +0530

    refactor: 15 cards only

--- src/components/NewsCards/LatestNews.tsx
+++ src/components/NewsCards/LatestNews.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NewsItems } from "../../utils/data";
 import CardItem from "./CardItem";
 import { cn } from "../../utils/cn";
-import './LatestNews.css';
+import "./LatestNews.css";
 
 export default function LatestNews() {
   const containerRef = React.useRef<HTMLDivElement>(null);
@@ -29,7 +29,7 @@ export default function LatestNews() {
         "--animation-direction",
         "forwards"
       );
-      containerRef.current.style.setProperty("--animation-duration", "360s");
+      containerRef.current.style.setProperty("--animation-duration", "150s");
       setStart(true);
     }
   }
@@ -37,14 +37,14 @@ export default function LatestNews() {
   const scrollToStart = () => {
     if (scrollerRef.current) {
       // Stop the animation
-      scrollerRef.current.style.animationPlayState = 'paused';
+      scrollerRef.current.style.animationPlayState = "paused";
 
       // Reset scroll position
       scrollerRef.current.scrollLeft = 0;
 
       // Reset the animation by removing it and then re-adding it
       const originalAnimation = scrollerRef.current.style.animation;
-      scrollerRef.current.style.animation = 'none';
+      scrollerRef.current.style.animation = "none";
 
       // Force reflow to ensure the animation reset is applied
       // This is a trick to force the browser to recognize the change and apply it immediately
@@ -65,11 +65,11 @@ export default function LatestNews() {
 
   const handleMouseEnter = () => {
     setIsHovering(true);
-  }
+  };
 
   const handleMouseLeave = () => {
     setIsHovering(false);
-  }
+  };
 
   return (
     <div
@@ -79,18 +79,23 @@ export default function LatestNews() {
       <h2 className="text-5xl text-white pl-20 pt-20 pb-10 font-space">
         Latest News
       </h2>
-      <button className="scroll-to-start-btn arrowMove absolute top-28 right-10 h-9 w-9 text-white"
-      onClick={scrollToStart}
+      <button
+        className="scroll-to-start-btn arrowMove absolute top-28 right-10 h-9 w-9 text-white"
+        onClick={scrollToStart}
       >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" style={{ width: '100%', height: '100%' }}>
-        <title>4-Arrow Left</title>
-        <g id="_4-Arrow_Left" data-name="4-Arrow Left">
-          <path
-            fill="white"
-            d="M32,15H3.41l8.29-8.29L10.29,5.29l-10,10a1,1,0,0,0,0,1.41l10,10,1.41-1.41L3.41,17H32Z"
-          />
-        </g>
-      </svg> 
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 32 32"
+          style={{ width: "100%", height: "100%" }}
+        >
+          <title>4-Arrow Left</title>
+          <g id="_4-Arrow_Left" data-name="4-Arrow Left">
+            <path
+              fill="white"
+              d="M32,15H3.41l8.29-8.29L10.29,5.29l-10,10a1,1,0,0,0,0,1.41l10,10,1.41-1.41L3.41,17H32Z"
+            />
+          </g>
+        </svg>
       </button>
       <ul
         className={cn(
@@ -103,9 +108,12 @@ export default function LatestNews() {
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        {NewsItems.map((item) => (
-          <CardItem key={item.title} cardItem={item} className="m-1" />
-        ))}
+        {NewsItems.map(
+          (item, index) =>
+            index < 15 && (
+              <CardItem key={index} cardItem={item} className="m-1" />
+            )
+        )}
       </ul>
     </div>
   );
