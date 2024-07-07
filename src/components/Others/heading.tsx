9b4cbd6b94fd558d5b4142217c2524b4e7ef9f2c
TheBug <atharvsathe28704@gmail.com>
2024-07-07 23:13:28 +0530

    Modified UI

--- src/components/Others/heading.tsx
+++ src/components/Others/heading.tsx
@@ -1,7 +1,9 @@
-export default function Heading(){
-    return (
-        <div className="flex m-0 items-center justify-center">
-            <h1 className="lg:text-5xl text-4xl text-white font-space my-10">Isro Centers And Units</h1>
-        </div>
-    )
+export default function Heading() {
+  return (
+    <div className="flex m-0 items-center justify-center">
+      <h1 className="lg:text-5xl text-4xl text-white text-center font-space my-10">
+        Isro Centers And Units
+      </h1>
+    </div>
+  );
 }
